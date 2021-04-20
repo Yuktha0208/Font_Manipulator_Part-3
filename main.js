1b79c06f83c0c0fd511aba8ec920f0be2573be69
Yuktha0208 <71544862+Yuktha0208@users.noreply.github.com>
2021-04-20 18:36:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+//function setup(){
+   // video=createCapture(VIDEO);
+    //video.size(550, 500);
+    //canvas=createCanvas(460,460);
+    //canvas.position(560,90);
+    //poseNet= ml5.poseNet(video, modelLoaded);
+    //poseNet.on("pose", gotPoses);
+//}
+//function modelLoaded(){
+   // console.log("Your Model Is Initialized");
+//}
+//function gotPoses(results){
+    //if(results.length >0){
+       // console.log(results);
+    //}
+//}
+
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
+
+  canvas = createCanvas(450, 450);
+  canvas.position(560,130);
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized!');
+}
+
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
+}
+
+function draw() {
+background('#6C91C2');
+
+  document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px";
+textSize(difference);
+fill('#FFE787');
+text('Peter', 50, 400);
+}
